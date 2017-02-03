afa9ba0cfaab95df53bea63904fd8b396d557dc8
Niinarie <pocky.hero@gmail.com>
2017-02-03 11:07:49 +0200

    advanced works!

--- src/app/advanced-search/advanced-search.component.ts
+++ src/app/advanced-search/advanced-search.component.ts
@@ -1,4 +1,6 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-advanced-search',
@@ -7,7 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdvancedSearchComponent implements OnInit {
 
-  constructor() { }
+  private intolerancesArray = [
+    {value: 'dairy', checked: false },
+    {value: 'egg', checked: false },
+    {value: 'wheat', checked: false }
+  ];
+
+  public searchForm = this.fb.group({
+    query: [''],
+    excludeIngredients: [''],
+    intolerances: []
+  });
+
+
+  constructor(public fb: FormBuilder, private router: Router) { }
+
+  search(event) {
+
+    let iArray = [];
+
+     this.intolerancesArray
+        .filter(opt => opt.checked)
+        .map(opt => opt.value)
+        .map(function(opt){
+          iArray.push(opt)
+        });
+
+    console.log(iArray);
+
+    let query = this.searchForm.value;
+    query.intolerances = iArray;
+
+
+   // console.log(query);
+    event.preventDefault();
+    this.router.navigate(['search', query]);
+  }
+
 
   ngOnInit() {
   }
