04890c3f72dcb7a1d6654426fa9cefb37ed29086
Niinarie <pocky.hero@gmail.com>
2017-02-14 09:10:45 +0200

    added old adv search for the sake of it

--- src/app/advanced-search/advanced-search.component.ts
+++ src/app/advanced-search/advanced-search.component.ts
@@ -1,4 +1,6 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-advanced-search',
@@ -7,7 +9,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdvancedSearchComponent implements OnInit {
 
-  constructor() { }
+  private chosenType: string = '';
+  checkTypes(){
+      this.chosenType = this.searchForm['type'];
+  }
+
+   private dietArray = [
+     'vegan',
+     'vegetarian'
+   ];
+
+  private intolerancesArray = [
+    {value: 'dairy', checked: false },
+    {value: 'gluten', checked: false },
+    {value: 'egg', checked: false },
+    {value: 'peanut', checked: false },
+    {value: 'soy', checked: false },
+    {value: 'seafood', checked: false },
+    {value: 'shellfish', checked: false },
+    {value: 'sulfite', checked: false },
+    {value: 'tree nut', checked: false },
+    {value: 'wheat', checked: false }
+  ];
+
+  private typesArray = [
+    'main course',
+    'side dish',
+    'dessert',
+    'appetizer',
+    'salad',
+    'bread',
+    'breakfast',
+    'soup',
+    'beverage',
+    'sauce',
+    'drink'
+  ];
+
+
+  public searchForm = this.fb.group({
+    query: [''],
+    excludeIngredients: [''],
+    intolerances: [],
+    type: [''],
+    diet: ['']
+  });
+
+
+  constructor(public fb: FormBuilder, private router: Router) { }
+
+  search(event) {
+
+    let iArray = [];
+
+     this.intolerancesArray
+        .filter(opt => opt.checked)
+        .map(opt => opt.value)
+        .map(function(opt){
+          iArray.push(opt)
+        });
+
+    console.log(iArray);
+
+    let query = this.searchForm.value;
+
+    query.intolerances = iArray;
+    query.type = query.type.replace(/ /,"+");
+
+
+   // console.log(query);
+    event.preventDefault();
+    this.router.navigate(['search', query]);
+  }
+
 
   ngOnInit() {
   }
