f89d193c7cde08d6e47e875d8fdbb97683269cc6
Niinarie <pocky.hero@gmail.com>
2017-02-28 13:49:53 +0200

    merged comments

--- src/app/commentbox/commentbox.component.ts
+++ src/app/commentbox/commentbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-commentbox',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./commentbox.component.scss']
 })
 export class CommentboxComponent implements OnInit {
+
+  @Input() recipeURL: any;
 
   constructor() { }
 
